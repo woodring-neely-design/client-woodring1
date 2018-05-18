b4ea43f42e2104a0d256c9d1d986579464cbc6db
Alden Neely <alden.s.neel@gmail.com>
2018-05-18 15:41:44 -0500

    idk work

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   addServicesExpansion();
   addNavbarScroll();
+
+  $("#services").bgswitcher({
+    images: ['./img/construction-3331438_1920.jpg', './img/Construction_Entrance.JPG', 
+      './img/1024px-Rebar_worker.jpg'],
+    loop: false
+  });
 })
+
 
 let addServicesExpansion = () => {
   $('#services span.title').click(function (){
     $('#services p').hide(); 
-    console.log(this);
+
     $(this).siblings('p').show();
+    const idx = $('#services span.title').index($(this));
+    $('#services').bgswitcher('select', idx);
   });
 }
 
