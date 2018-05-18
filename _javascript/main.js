172c90728d73c5f907b860c64d86843dc406d1cf
Alden Neely <alden.s.neel@gmail.com>
2018-05-18 13:29:15 -0500

    services page

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  addServicesExpansion();
+  addNavbarScroll();
 })
+
+let addServicesExpansion = () => {
+  $('#services span.title').click(function (){
+    $('#services p').hide(); 
+    console.log(this);
+    $(this).siblings('p').show();
+  });
+}
+
+let addNavbarScroll = () => {
+  $('a.navbar-item').click(function(){
+    scrollTo($(this).attr('href'));
+  });
+}
+
+let scrollTo = (selector) => {
+  $('html, body').animate({
+    scrollTop: $(selector).offset().top
+  }, 1500);
+}
