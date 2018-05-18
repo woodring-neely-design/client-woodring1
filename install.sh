f2af27433933d5d32865d8726f07994da96b4954
Alden Neely <alden.s.neel@gmail.com>
2018-05-18 13:33:32 -0500

    fixed conflict

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ if ! hash brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# install watch
+if ! hash watch; then
+  brew install watch
+fi
+
 # install node and npm 
 if ! hash node; then
   brew install node
