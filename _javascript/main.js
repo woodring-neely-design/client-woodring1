d5f6121dbb42a671fc4d2d6b1a02917a4c861192
Alden Neely <alden.s.neel@gmail.com>
2018-06-11 13:51:47 -0500

    fini

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,22 +1,35 @@
+const primary = '0, 65, 111';
+let skip1 = false;
+
 document.addEventListener('DOMContentLoaded', () => {
   addServicesExpansion();
   addNavbarScroll();
 
-  $("#services").bgswitcher({
-    images: ['./img/construction-3331438_1920.jpg', './img/Construction_Entrance.JPG', 
-      './img/1024px-Rebar_worker.jpg'],
-    loop: false
+  doScroll();
+
+  $("#services-img").bgswitcher({
+    images: ['./photos/Lynchburg.JPG', './photos/Columbia.JPG', 
+      './photos/Ardmore.jpg'],
+    loop: false,
+    start: false
   });
-})
+
+  $("#open-gallery").click(() => $(".modal").addClass("is-active"))
+  $(".modal-close").click(() => $(".modal").removeClass("is-active"))
+});
+
 
 
 let addServicesExpansion = () => {
   $('#services span.title').click(function (){
     $('#services p').hide(); 
 
+    $('#services span.title').removeClass("has-text-primary");
+    $(this).addClass("has-text-primary");
+
     $(this).siblings('p').show();
     const idx = $('#services span.title').index($(this));
-    $('#services').bgswitcher('select', idx);
+    $('#services-img').bgswitcher('select', idx);
   });
 }
 
@@ -27,7 +40,47 @@ let addNavbarScroll = () => {
 }
 
 let scrollTo = (selector) => {
+  skip1 = true;
   $('html, body').animate({
-    scrollTop: $(selector).offset().top
+    scrollTop: $(selector).offset().top - 52
   }, 1500);
+}
+
+function doScroll() {
+  const dist = window.scrollY / 300.0;
+  console.log(dist);
+
+  /*
+  const backgroundColor = `rgba(255, 255, 255, ${dist})`;
+  const borderColor = `rgba(${primary}, ${dist})`;
+  const borderBottom = `2px solid  ${borderColor}`;
+
+  setTimeout(() =>
+    $('nav').css({backgroundColor, borderBottom}),
+    30);
+    */
+}
+
+$(window).scroll(() => {
+  if(skip1) {
+    skip1 = false
+    return
+  }
+
+
+  lastDist = window.scrollY;
+  doScroll();
+});
+
+function toWhite(rgb, amt) {
+  return rgb
+    .split(',')
+    .map((it) => tween(parseInt(it), 255, amt))
+    .join(',');
+}
+
+function tween(start, end, amt) {
+  const hard = (amt > 1.0)?1.0:amt;
+  console.log(hard);
+  return start + (end - start) * hard;
 }
